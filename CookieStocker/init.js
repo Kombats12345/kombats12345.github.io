bf3d27c1a9d450bac21a292203cb5d8f37886204
Henos Yonas <41198431+Kombats12345@users.noreply.github.com>
2024-03-06 00:36:27 -0500

    Update init.js

--- CookieStocker/init.js
+++ CookieStocker/init.js
@@ -1,1 +1,1 @@
-Game.LoadMod('https://raw.githubusercontent.com/Kombats12345/cc-mods/main/CookieStocker/main.js');
+Game.LoadMod('https://raw.githubusercontent.com/Kombats12345/kombats12345.github.io/main/CookieStocker/main.js');
